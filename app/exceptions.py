c5811a7e8881eb4c73352dc7d9520aaa73253763
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-09-22 15:35:26 +0300

    Fix exceptions module;

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,9 +1,39 @@
 from fastapi import HTTPException, status
 
 
-UserAlreadyExistsException = HTTPException(status_code=status.HTTP_409_CONFLICT, detail='User already exists')
-UserDoesNotExistsException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='The user does not exists')
-JWTNotValidException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='JWT token is not valid')
-JWTExpiredException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='JWT token expired')
-JWTNotFoundException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='JWT not found')
-UserIDDoesNotExistsException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User id does not exists')
+class BookingException(HTTPException):
+    status_code = 500
+    detail = ''
+
+    def __init__(self):
+        super().__init__(status_code=self.status_code, detail=self.detail)
+
+
+class UserAlreadyExistsException(BookingException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = 'User already exists'
+
+
+class UserDoesNotExistsException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'The user does not exists'
+
+
+class JWTNotValidException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'JWT token is not valid'
+
+
+class JWTExpiredException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'JWT token expired'
+
+
+class JWTNotFoundException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'JWT not found'
+
+
+class UserIDDoesNotExistsException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'User id does not exists'
