bda2d90839c06cceb4f4b6f830c429604975f9ab
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-09-25 11:45:43 +0300

    Add query for getting available rooms;

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,6 +1,53 @@
+from sqlalchemy import select
+
 from app.bookings.models import Bookings
 from app.dao.base import BaseDAO
+from app.database import async_session_maker, engine
+from sqlalchemy import and_, or_, func
+
+from app.rooms.models import Rooms
 
 
 class BookingDAO(BaseDAO):
     model = Bookings
+
+    @classmethod
+    async def add_booking(cls, user_id: int, room_id: int, date_from, date_to):
+        async with async_session_maker() as session:
+            '''
+            with booked_rooms as 
+            (
+            select * from bookings where room_id = 1 and (date_from >= '2023-05-15' and date_from <= '2023-06-20')
+            or 
+            date_from <= '2023-05-15' and date_to > '2023-05-15'
+            )
+            '''
+            booked_rooms = select(Bookings).where(
+                and_(
+                    Bookings.room_id == room_id,
+                    or_(
+                        and_(
+                            Bookings.date_from >= date_from,
+                            Bookings.date_from <= date_to
+                        ),
+                        and_(
+                            Bookings.date_from <= date_from,
+                            Bookings.date_to > date_from
+                        )
+                    )
+                )
+            ).cte('booked_rooms')
+
+            '''
+            select rooms.quantity - count(booked_rooms.room_id) from rooms left join booked_rooms on 
+            booked_rooms.room_id = rooms.id where rooms.id = 1 group by rooms.quantity, booked_rooms.room_id
+            '''
+            available_rooms = select(
+                (Rooms.quantity - func.count(booked_rooms.c.room_id)).label('available_rooms')
+                 ).select_from(Rooms).join(booked_rooms, booked_rooms.c.room_id == Rooms.id).where(Rooms.id == room_id).group_by(Rooms.quantity, booked_rooms.c.room_id)
+            # print(available_rooms.compile(engine, compile_kwargs={'literal_binds': True}))
+            result = await session.execute(available_rooms)
+            return result.mappings().all()
+
+
+
--- app/bookings/router.py
+++ app/bookings/router.py
@@ -3,12 +3,19 @@ from app.bookings.dao import BookingDAO
 from app.bookings.schemas import SBooking
 from app.users.models import Users
 from app.users.dependencies import get_current_user
+from datetime import date
 
 
 router = APIRouter(prefix='/bookings', tags=['Bookings'])
 
 
 @router.get('')
-async def get_bookings(user: Users = Depends(get_current_user)):
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[SBooking]:
     result = await BookingDAO.get_all_bookings(user_id=user.id)
+    return result
+
+
+@router.post('')
+async def add_booking(room_id: int, date_from: date, date_to: date, user: Users = Depends(get_current_user)):
+    result = await BookingDAO.add_booking(user_id=user.id, room_id=room_id, date_from=date_from, date_to=date_to)
     return result
