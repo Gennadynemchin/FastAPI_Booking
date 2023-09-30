d2ebad24cf38deb033c0bc5594678cd8194595f4
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-09-30 16:12:09 +0300

    Fix get_available_hotels query result;

--- app/hotels/dao.py
+++ app/hotels/dao.py
@@ -9,7 +9,7 @@ from app.dao.base import BaseDAO
 from app.database import async_session_maker
 from app.hotels.models import Hotels
 from app.hotels.rooms.models import Rooms
-from sqlalchemy import select, distinct
+from sqlalchemy import select, distinct, cast, String
 from datetime import date
 
 
@@ -19,7 +19,7 @@ class HotelsDAO(BaseDAO):
     @classmethod
     async def find_available(cls, location: str, date_from: date, date_to: date):
         async with async_session_maker() as session:
-            get_hotels_by_location = select(cls.model.__table__.columns).where(cls.model.location.contains(location))
+            # get_hotels_by_location = select(cls.model.__table__.columns).where(cls.model.location.contains(location))
 
             count_booked = select(Bookings.room_id, func.count(Bookings.room_id).label('count_booked')).where(
                     or_(
@@ -40,20 +40,20 @@ class HotelsDAO(BaseDAO):
                 count_booked, count_booked.c.room_id == Rooms.id, isouter=True).group_by(Rooms.id, available_rooms).having(available_rooms > 0).cte('get_available_rooms')
 
             get_available_hotels = select(
-                                          Hotels.id,
-                                          Hotels.name,
-                                          Hotels.location,
-                                          Hotels.services,
-                                          Hotels.rooms_quantity,
-                                          get_available_rooms.c.available_rooms,
-                                          Hotels.image_id
-                                         ).select_from(
-                                                       Hotels
-                                                      ).where(
-                                                              Hotels.location.contains(location)
-                                                             ).join(
-                                                                    get_available_rooms,
-                                                                    get_available_rooms.c.hotel_id == Hotels.id).group_by(Hotels.id, get_available_rooms.c.available_rooms)
+                Hotels.id,
+                Hotels.name,
+                Hotels.location,
+                Hotels.services,
+                Hotels.rooms_quantity,
+                func.sum(get_available_rooms.c.available_rooms).label('rooms_left'),
+                Hotels.image_id
+            ).select_from(
+                Hotels
+            ).where(
+                Hotels.location.contains(location)
+            ).join(
+                get_available_rooms,
+                get_available_rooms.c.hotel_id == Hotels.id).group_by(Hotels.id)
 
             result = await session.execute(get_available_hotels)
             return result.mappings().all()
