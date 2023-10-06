85b4195c94e6f739b69c32a3ca68bf4b6edf8f02
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-10-06 23:19:49 +0300

    Add get hotel info router;

--- app/hotels/rooms/router.py
+++ app/hotels/rooms/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from app.hotels.rooms.schemas import SRooms
 from app.hotels.rooms.dao import RoomsDAO
+from app.hotels.dao import HotelsDAO
 from datetime import date
 
 
@@ -10,4 +11,10 @@ router = APIRouter(prefix='/hotels', tags=['Get rooms of requested hotel'])
 @router.get('/{hotel_id}/rooms')
 async def get_rooms(hotel_id: int, date_from: date, date_to: date) -> list[SRooms]:
     result = await RoomsDAO.get_all_rooms(hotel_id, date_from, date_to)
+    return result
+
+
+@router.get('/id/{hotel_id}')
+async def get_hotel(hotel_id: int):
+    result = await HotelsDAO.get_one_or_none(id=hotel_id)
     return result
