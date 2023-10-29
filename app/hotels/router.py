9e6c1431452c048a558988f336ad8128c2302ce3
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-10-29 13:30:22 +0300

    Add redis cache;

--- app/hotels/router.py
+++ app/hotels/router.py
@@ -1,14 +1,19 @@
 from fastapi import APIRouter
+from fastapi_cache.decorator import cache
+
 from app.hotels.dao import HotelsDAO
 from app.hotels.schemas import SHotels
 from datetime import date
+
+import asyncio
 
 
 router = APIRouter(prefix='/hotels')
 
 
 @router.get('/{location}')
+@cache(expire=100)
 async def get_hotels(location: str, date_from: date, date_to: date) -> list[SHotels]:
-
+    # await asyncio.sleep(3)
     result = await HotelsDAO.find_available(location, date_from, date_to)
     return result
--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+
+from redis import asyncio as aioredis
 
 from app.users.router import router as router_auth
 from app.bookings.router import router as router_bookings
@@ -17,3 +24,20 @@ app.include_router(router_hotels)
 app.include_router(router_rooms)
 app.include_router(router_upload)
 
+
+origins = ['http://127.0.0.1:8000', ]
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=['GET', 'POST', 'OPTIONS', 'DELETE', 'PATCH', 'PUT'],
+                   allow_headers=['Content-Type',
+                                  'Set-Cookie',
+                                  'Access-Control-Allow-Headers',
+                                  'Access-Authorization'],
+                   )
+
+
+@app.on_event("startup")
+async def startup():
+    redis = aioredis.from_url("redis://localhost:6379")
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
